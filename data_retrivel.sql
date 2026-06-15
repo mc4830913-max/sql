a9e90493a80cf52d57424b9c9f200d6370ab0795
Monika Chauhan <mc4830913@gmail.com>
2026-06-15 07:18:45 +0530

    Add SQL queries for data retrieval from datasets
    Added multiple SQL queries to retrieve data from clean_olist_order_payment_dataset and clean_olist_products_dataset with various conditions.

---
+++ data_retrivel.sql
@@ -0,0 +1,47 @@
+use newschema;
+select * from clean_olist_order_payment_dataset;
+select order_id,payment_value  from  clean_olist_order_payment_dataset;
+
+select order_id as ID from clean_olist_order_payment_dataset;
+
+select * from clean_olist_order_payment_dataset  where payment_value >100;
+
+select * from clean_olist_order_payment_dataset  where payment_value >100 and payment_category="single";
+
+select * from clean_olist_order_payment_dataset  where payment_value >100 and payment_value < 200;
+
+select * from clean_olist_order_payment_dataset  where payment_value between 100 and 200;
+
+select * from clean_olist_order_payment_dataset  where payment_type= "boleto" or payment_type= "voucher"; 
+
+select * from clean_olist_products_dataset where product_category_name like "be%"
+
+select * from clean_olist_products_dataset where product_category_name="b%" or product_category_name like "%e"
+
+select * from clean_olist_products_dataset limit 5;
+
+select * from clean_olist_products_dataset limit 5 offset 10;
+
+select * from clean_olist_products_dataset where product_category_name="bebes" limit 5 ;
+
+select * from clean_olist_products_dataset order by product_weight_g desc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
