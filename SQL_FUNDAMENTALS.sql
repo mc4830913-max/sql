d3843b78d8abf6e615c0ef07730f7863463e4e69
Monika Chauhan <mc4830913@gmail.com>
2026-06-15 21:00:55 +0530

    Update SQL_FUNDAMENTALS.sql

--- SQL_FUNDAMENTALS.sql
+++ SQL_FUNDAMENTALS.sql
@@ -28,6 +28,82 @@ select * from clean_olist_products_dataset where product_category_name="bebes" l
 
 select * from clean_olist_products_dataset order by product_weight_g desc;
 
+-------------------------------------------------------------------------------------------------------------------------------------------------
+-- caese in sql 
+
+use newschema;
+
+select * from clean_olist_products_dataset;
+
+select product_category_name as name  from clean_olist_products_dataset;
+
+-- --basic case 
+select product_id,
+case product_category_name
+     when "bebes" then "bebes category"
+     else "other"
+end as name  from clean_olist_products_dataset;
+
+-- caese for the bebes and perfumia 
+select product_id,
+case product_category_name
+     when "bebes" then "bebes category"
+     when "perfumaria" then "perfumaria category"
+     else "other"
+end as name  from clean_olist_products_dataset;
+
+-- caese with quantity 
+select product_id,
+case 
+     when product_photos_qty =1  then "Low-volume customer "
+     when product_photos_qty >=2 and product_photos_qty<=5 then "medium-volume customer"
+     else "high-volume customer"
+end as quantity  from clean_olist_products_dataset;
+
+-- nested case
+select product_id,
+case 
+     when product_photos_qty =1 then
+		case 
+			when product_category_name ="bebes" then "low volume bebes category"
+            else "low category"
+            end
+     when product_photos_qty >=2 and product_photos_qty<=5 then "medium-volume customer"
+     else "high-volume customer"
+end as quantity  from clean_olist_products_dataset;
+
+
+-- null caeses
+select product_id,
+case 
+     when product_category_name is null then "not assigned product name"
+     else product_category_name 
+end as quantity  from clean_olist_products_dataset;
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
