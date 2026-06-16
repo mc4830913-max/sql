56e7058e5fd7a57bc82ac3a53dffcbf0727272b4
Monika Chauhan <mc4830913@gmail.com>
2026-06-16 06:53:31 +0530

    Update ddl_command.sql

--- ddl_command.sql
+++ ddl_command.sql
@@ -45,3 +45,60 @@ select * from Student;
 -- adding new column in table by default value
 alter table Student modify column gmail varchar(50) default "not provided ";
 select * from Student;
+
+
+------------------------------------------------------------------------------------------------------------------------------------------
+-- keys and constraints
+
+-- primary key 
+drop table if exists Student;
+create table if not exists Student(
+             roll_no serial primary key,
+             name varchar(50),
+             department varchar(50));
+select * from Student;             
+ 
+ -- composite key 
+drop table if exists orders;
+create table orders(
+ order_id serial,
+ customer_id int,
+ puchase_date date,
+ primary key(order_id,customer_id)
+ );
+           
+ select * from orders; 
+ 
+ -- foreign key
+drop table if exists orders;
+create table orders(
+ order_id serial,
+ customer_id int,
+ puchase_date date,
+foreign key(customer_id) references Student(roll_no)
+ );
+           
+ select * from orders; 
+ 
+ -- unique constraint 
+drop table if exists Student;
+create table if not exists Student(
+             roll_no serial primary key,
+             name varchar(50),
+             department varchar(50),
+             email varchar(50) unique,
+             phone_no int(15) unique
+             );
+select * from Student;   
+ 
+ -- check constraint 
+drop table if exists Student;
+create table if not exists Student(
+             roll_no serial primary key,
+             name varchar(50),
+             department varchar(50),
+             age int check(age>=18));
+select * from Student;   
+ 
+ 
+ 
