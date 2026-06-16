faa6ee0e9726ff9523e6143ad8c411e3c42dc5d8
Monika Chauhan <mc4830913@gmail.com>
2026-06-16 07:25:29 +0530

    Update ddl_command.sql

--- ddl_command.sql
+++ ddl_command.sql
@@ -100,5 +100,64 @@ create table if not exists Student(
              age int check(age>=18));
 select * from Student;   
  
+ -------------------------------------------------------------------------------------------------------------------------------------------
+ -- inseting the data
+
+drop table if exists Student;
+create table if not exists Student(
+             roll_no serial primary key,
+             first_name varchar(50),
+             last_name varchar(50),
+             department varchar(50),
+             age int check(age >18),
+             addmission_date date 
+             );
+select * from Student;             
  
+ -- inset value in single rows
+ insert into Student(roll_no , first_name,last_name, department,age,addmission_date)
+ values(1,"monika","chauhan","CSE",23,'2022-11-20');
+ select * from Student;
  
+  -- inset value in multiplr rows rows
+ insert into Student(roll_no , first_name,last_name, department,age,addmission_date)
+ values(2,"Anushka","sharma","ECE",20,'2022-11-10'),
+ (3,"priya","mittal","CE",22,'2022-11-05'),
+ (4,"pavel","patel","CIVIL",21,'2022-11-02'),
+ (5,"rohit","sharma","CSE",23,'2022-11-04');
+ select * from Student;
+ 
+ -- insert the partial data 
+  insert into Student(roll_no , first_name,last_name)
+ values(6,"Aakash","prajapati");
+ select * from Student;
+ 
+ -- insert data without orders
+insert into Student(addmission_date, last_name,first_name, roll_no)
+ values("2022-11-13","kashyap","laxmi",7);
+ select * from Student;
+
+
+-- adding cuurent date and default values
+drop table if exists default_table;
+create table if not exists default_table(
+             roll_no serial primary key,
+             name varchar(50) not null,
+             department varchar(50) default "not filled",
+             addmission_date timestamp default current_timestamp
+             );
+select * from default_table;  
+ 
+ -- insert single valuse
+ insert into default_table(name) values("Monika");
+ select * from default_table;
+ 
+  -- insert multiple values valuse
+ insert into default_table(name) values("anuskha"),("preety"),("laxmi"),("komal"),("hussan"),("chetna");
+ select * from default_table;
+ 
+ insert into default_table(name,department) values("anu","cse"),("priya","Bca"),("lovi","Bba"),("kamal","Ce");
+ select * from default_table
+ 
+insert into default_table(name,addmission_date) values("pragati","2022-11-29");
+select * from default_table;
