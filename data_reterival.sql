c0e813cca2d44c93d77268df33ec339396619233
Monika Chauhan <mc4830913@gmail.com>
2026-06-18 22:30:58 +0530

    Add SQL queries for pattern matching on products

--- data_reterival.sql
+++ data_reterival.sql
@@ -1,1 +1,26 @@
 -- pattern matching
+use myntra;
+
+-- find the product with brand name end with a
+select * from data where brand_name like "%a";
+
+-- find the product with brand name start with s
+select * from data where brand_name like "s%";
+
+-- find the product with brand name contain as
+select * from data where brand_name like "%as%";
+
+-- find the product with brand anme contain ash
+select * from data where product_name like "%ash%";
+
+-- find the product with brand name end with a
+select * from data where brand_name like "%a%b";
+
+-- find the product with product name end with a
+select * from data where product_name like "d%a";
+
+-- find the product with brand name is exactly 6 character
+select * from data where brand_name like "_____";
+
+-- find the product with product name whose 2nd character is fuxed
+select * from data where product_name like "_a_d%";
